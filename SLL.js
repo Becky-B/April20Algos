d96ae4aefb5efbd8c007ff372bcfbb342dcff66b
Cody Thaller <stevencthaller@gmail.com>
2020-04-07 09:43:15 -0700

    First commit

---
+++ SLL.js
@@ -0,0 +1,24 @@
+class SLNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SLList {
+    constructor() {
+        this.head = null;
+    }
+
+    // Add to front
+    addToFront(value) {
+
+        return this;
+    }
+
+
+    // Remove from front
+    removeFromFront() {
+
+    }
+}
