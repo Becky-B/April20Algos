403e44f4da2ae387248e4d871e9131a0e9773c1e
Cody Thaller <stevencthaller@gmail.com>
2020-04-10 08:50:04 -0700

    MinToFront and RemoveVal added

--- SLL.js
+++ SLL.js
@@ -10,6 +10,21 @@ class SLList {
         this.head = null;
     }
 
+    printList() {
+        if(this.head == null) {
+            console.log("The list is empty.");
+            return this;
+        }
+        let str = "H: ";
+        let runner = this.head;
+        while(runner != null) {
+            str += `${runner.value} -> `
+            runner = runner.next;
+        }
+        console.log(str);
+        return this;
+    }
+
     // Add to front
     addToFront(value) {
         // create the new node we want to add to the front
@@ -43,11 +58,106 @@ class SLList {
     // Contains. Challenge --> Do this recursively
     contains(value) {
         // return a boolean: if the value is in the list, return true, else return false
+        if(this.head == null){
+            return false;
+        }
+        // Set our runner to traverse through the list
+        let runner = this.head;
+        while(runner != null) {
+            // check if the runner's value matches the value we're searching for
+            if(runner.value == value) {
+                return true;
+            }
+            // move it along
+            runner = runner.next;
+        }
+        // if we've reached the end of our list and NOT found the value, then it must not be in here
+        return false;
+    }
+    // same as above but recursively
+    rContains(value, runner = this.head) {
+        // check for empty list or end of list because of recursion
+        if(runner == null) {
+            return false;
+        }
+        // check if runner's value is the value we're looking for
+        else if(runner.value == value) {
+            return true;
+        }
+        // make recursive call and return it so as soon as the end of the call stack finishes it shoots back up the chain
+        return this.rContains(value, runner.next);
     }
 
 
     // Remove from the back of a singly linked list
     removeFromBack() {
+        if(this.head == null) {
+            return this;
+        }
+        else if(this.head.next == null) {
+            this.head = null;
+            return this;
+        }
+        let runner = this.head;
+        let walker = null;
+        while(runner.next != null) {
+            walker = runner;
+            runner = runner.next;
+        }
+        walker.next = null;
+        return this;
+    }
+
+    // Move the smallest number to the front of the singly linked list
+    moveMinToFront() {
+        if(this.head == null) {
+            return this;
+        }
+        let runner = this.head;
+        let walker = null;
+        let min = this.head;
+        let minprev = null;
+
+        while(runner.next != null) {
+            if(runner.value < min.value) {
+                min = runner;
+                minprev = walker;
+            }
+            walker = runner;
+            runner = runner.next;
+        }
+        if(min == this.head) {
+            return this;
+        }
+        minprev.next = min.next;
+        min.next = this.head;
+        this.head = min;
+        return this;
+    }
+
+    // Remove the first node with the given value from the list
+    removeVal(value) {
+        if(this.head == null) {
+            console.log("List is empty");
+            return this;
+        }
+        else if(this.head.value == value) {
+            this.head = this.head.next;
+            return this;
+        }
+        let runner = this.head;
+        let walker = null;
+        while(runner != null) {
+            if(runner.value == value) {
+                walker.next = runner.next;
+                return this;
+            }
+            walker = runner;
+            runner = runner.next;
+        }
+        console.log("Value not found.")
+        return this;
 
     }
 }
+
