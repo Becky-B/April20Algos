7ba15683eaa4c43c7e3d9a2eb8a3a3cbe6787474
Cody Thaller <stevencthaller@gmail.com>
2020-04-22 08:34:26 -0700

    Doubly Linked List

---
+++ DLL.js
@@ -0,0 +1,31 @@
+class DLNode {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DLList {
+    constructor() {
+        this.head = null;
+    }
+
+
+    // Push to the end of the current DLList
+    push(value) {
+
+    }
+
+
+    // Remove the last element of the DLList
+    pop() {
+
+    }
+
+    
+    // Remove a specific value from a DLList
+    remove(value) {
+
+    }
+}
