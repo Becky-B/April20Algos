441571500ad4c3e731dc736511ce7892a72f0b82
Cody Thaller <stevencthaller@gmail.com>
2020-04-28 10:14:23 -0700

    Solutions for 4/28 added

--- BST.js
+++ BST.js
@@ -16,23 +16,47 @@ class BSTree {
 
     // write an algorithm that can determine whether or not a binary search tree is empty.
     isEmpty() {
-
+        if(this.root == null) {
+            return true;
+        }
+        return false;
     }
 
 
     // write an algorithm that will find the smallest number in a binary search tree
     min(){
-
+        if(this.isEmpty()) {
+            return false;
+        }
+        let runner = this.root;
+        while(runner.left != null) {
+            runner = runner.left;
+        }
+        return runner.value;
     }
 
     // write an algorithm that will find the largest number in a binary search tree
     max() {
-
+        if(this.isEmpty()) {
+            return false;
+        }
+        let runner = this.root;
+        while(runner.right != null) {
+            runner = runner.right;
+        }
+        return runner.value;
     }
 
     // 4/28 BONUS: Write an algorithm that can determine whether or not the binary search tree
     // contains a node with a given value
-    contains(value) {
-
+    contains(value, runner = this.root) {
+        if(runner == null)
+            return false;
+        else if(value == runner.value)
+            return true;
+        else if(value < runner.value)
+            return this.contains(value, runner.left);
+        else 
+            return this.contains(value, runner.right);
     }
 }
